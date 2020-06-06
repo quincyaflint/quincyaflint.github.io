6f13394031365ecfaa77174d30cb84221f360343
qflint <quincyaflint@hotmail.com>
2020-06-05 22:24:59 -0400

    Update Friday 10:24 pm

--- js/boggle-new-js.js
+++ js/boggle-new-js.js
@@ -1,5 +1,13 @@
-/* Initialize the Game */
-var list_of_die = [
+
+// GLOBAL VARIABLES
+var dice_list_4 = [
+'ARELSC','TABIYL','EDNSWO','BIOFXR',
+ 'MCDPAE','IHFYEE','KTDNUO','MOQAJB',
+ 'ESLUPT','INVTGE','ZNDVAE','UKGELY',
+ 'OCATAI','ULGWIR','SPHEIN','MSHARO'
+];
+
+var dice_list_5 = [
 'AAAFRS','AAEEEE','AAFIRS','ADENNN','AEEEEM',
 'AEEGMU','AEGMNN','AFIRSY','BJKQXZ','CCNSTW',
 'CEIILT','CEILPT','CEIPST','DDLNOR','DHHLOR',
@@ -7,82 +15,49 @@ var list_of_die = [
 'FIPRSY','GORRVW','HIPRRY','NOOTUW','OOOTTU'
 ];
 
-var list_of_die_2 = [
-'ARELSC','TABIYL','EDNSWO','BIOFXR',
- 'MCDPAE','IHFYEE','KTDNUO','MOQAJB',
- 'ESLUPT','INVTGE','ZNDVAE','UKGELY',
- 'OCATAI','ULGWIR','SPHEIN','MSHARO'
-];
-
-var board_generator = [];
-var current_track = [];
-
 // START GAME!
-start_game();
+start_game(5);
 
 
-function start_game() {
-	board_generate();
-	board();
-}
-
-function board(){
-	var board = [];
-	var board_temp = [];
-	var dice_arr = [];
-	var upside;
-	var row;
-	var character='';
-	shuffle(board_generator);
-	for(let i=0;i<board_generator.length;i++){
-		dice_arr = board_generator[i];
-		upside = random_face(dice_arr);
-		if(upside==='Q')upside = 'Qu';
-		board_temp.push(upside);
+// FUNCTION: START GAME
+function start_game(n){
+	var num_dice = n*n;
+	var dice_grid = [];
+	var dice_list = [];
+	var die_face;
+	var t1, t2;
+	
+	if (n==4){dice_list=dice_list_4};
+	if (n==5){dice_list=dice_list_5};
+	
+	
+	// SHUFFLE DICE
+	for(let i=n;i>0;i--){
+		t1 = Math.floor(Math.random()*i);
+		t2 = dice_list[i-1];
+		dice_list[i-1] = dice_list[t1];
+		dice_list[t1] = t2;
 	}
-	for(let i=0;i<board_temp.length;i=i+5){
-		line = [];
-		line.push(board_temp[i]);
-		line.push(board_temp[i+1]);
-		line.push(board_temp[i+2]);
-		line.push(board_temp[i+3]);
-		line.push(board_temp[i+4]);
-		board.push(line);
+	
+	
+	// PICK FACE OF EACH DIE
+	for(let i=0;i<num_dice;i++){
+		die_face = dice_list[i][Math.floor(Math.random()*6)];
+		if(die_face==='Q'){die_face = 'Qu'};
+		dice_grid.push(die_face);
 	}
-
-	//render board on HTML
-	for(var row=0;row<5;row++){
-		for(var col=0;col<5;col++){
-			character = board[row][col];
-			var button = "<button type='button' class='btn dice'"+"row="+row+" col="+col+">"+ character + "</button>";
+	
+	
+	// RENDER BOARD IN HTML
+	for(var row=0;row<n;row++){
+		for(var col=0;col<n;col++){
+			die_face = dice_grid[row*n+col];
+			var button = "<button type='button' class='btn dice'"+"row="+row+" col="+col+">"+ die_face + "</button>";
 			var row_selector = document.getElementById("row"+row);
 			row_selector.innerHTML += button;
+				
 		}
 	}
-
-}
-
-function board_generate(){
-	var dice;
-	for(let i=0;i<list_of_die.length;i++){
-		dice = list_of_die[i].split('');
-		board_generator.push(dice);
-	}
-}
-
-function shuffle(arr){
-	var j,temp;
-	for(let i=arr.length;i>0;i--){
-		j=Math.floor(Math.random()*i);
-		temp = arr[i-1];
-		arr[i-1] = arr[j];
-		arr[j] = temp;
-	}
-}
-
-function random_face(arr){
-	var index = Math.floor(Math.random()*6);
-	var upside_face = arr[index];
-	return upside_face;
+	
 }
 
