75590a53968884735f3dd671c273ee9afd1e067d
qflint <quincyaflint@hotmail.com>
2020-06-10 15:24:25 -0400

    Update Wednesday 3:24 pm

--- js/boggle-n-js.js
+++ js/boggle-n-js.js
@@ -2,9 +2,9 @@
 // GLOBAL VARIABLES
 var dice_list_4 = [
 'ARELSC','TABIYL','EDNSWO','BIOFXR',
- 'MCDPAE','IHFYEE','KTDNUO','MOQAJB',
- 'ESLUPT','INVTGE','ZNDVAE','UKGELY',
- 'OCATAI','ULGWIR','SPHEIN','MSHARO'
+'MCDPAE','IHFYEE','KTDNUO','MOQAJB',
+'ESLUPT','INVTGE','ZNDVAE','UKGELY',
+'OCATAI','ULGWIR','SPHEIN','MSHARO'
 ];
 
 var dice_list_5 = [
@@ -28,25 +28,22 @@ function boggle_n(n){
 	if (n==5){dice_list=dice_list_5};
 	
 	// SHUFFLE DICE
-	for(let i=n;i>0;i--){
+	for(let i=num_dice;i>0;i--){
 		t1 = Math.floor(Math.random()*i);
 		t2 = dice_list[i-1];
 		dice_list[i-1] = dice_list[t1];
 		dice_list[t1] = t2;
 	}
 	
-	
 	// PICK FACE OF EACH DIE
 	for(let i=0;i<num_dice;i++){
 		die_face = dice_list[i][Math.floor(Math.random()*6)];
 		if(die_face==='Q'){die_face = 'Qu'};
 		dice_grid.push(die_face);
-	}
-	
+	}	
 	
 	// RENDER BOARD IN HTML
 	document.write('<p>');
-	// Establish table
 	
 	document.write('<table class="centered" style="border:2px solid #000;padding:20px;">');
 	//document.write('<h1>Timer: <span id="time1">02:30</span> </h1>');
